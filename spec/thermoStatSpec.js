d8d6cfb58fd4605a0e353c294e3271b787c8e07c
Ahsan <rasul.ahsan@gmail.com>
2021-09-03 16:26:04 +0100

    returns current energy usage

--- spec/thermoStatSpec.js
+++ spec/thermoStatSpec.js
@@ -1,57 +1,74 @@
-describe('themoStat', function() {
-  let thermostat;
+describe("themoStat", function () {
+	let thermostat;
 
-  beforeEach(() => {
-    thermostat = new thermoStat
-  })
-  it('starts with a temperature of 20', function() {
-    expect(thermostat.currentTemp).toEqual(20)
-  })
+	beforeEach(() => {
+		thermostat = new thermoStat();
+	});
+	it("starts with a temperature of 20", function () {
+		expect(thermostat.currentTemp).toEqual(20);
+	});
 
-  it('increases temp temperature with up()',() => {
-    thermostat.up()
-    expect(thermostat.currentTemp).toEqual(thermoStat.START_TEMPERATURE + 1)
-  })
+	it("increases temp temperature with up()", () => {
+		thermostat.up();
+		expect(thermostat.currentTemp).toEqual(thermoStat.START_TEMPERATURE + 1);
+	});
 
-  it('decreases temperature with down()', () => {
-    thermostat.down()
-    expect(thermostat.currentTemp).toEqual(thermoStat.START_TEMPERATURE - 1)
-  })
+	it("decreases temperature with down()", () => {
+		thermostat.down();
+		expect(thermostat.currentTemp).toEqual(thermoStat.START_TEMPERATURE - 1);
+	});
 
-  it('temperature cannot decrease beyond 10', () => {
-    minimumTemp = thermoStat.MINIMUM_TEMPERATURE
-    for (let i = 0; i < 11; i++) { 
-      thermostat.down()
-    }
-    expect(thermostat.currentTemp).not.toBeLessThan(minimumTemp)
-  })
+	it("temperature cannot decrease beyond 10", () => {
+		minimumTemp = thermoStat.MINIMUM_TEMPERATURE;
+		for (let i = 0; i < 11; i++) {
+			thermostat.down();
+		}
+		expect(thermostat.currentTemp).not.toBeLessThan(minimumTemp);
+	});
 
-  it('has a maximum temperature of 25 when in powersave mode', () => {
-    powersaveMaxTemp = thermoStat.POWER_SAVE_MAX_TEMP
-    for (let i = 0; i < 6; i++){
-      thermostat.up()
-    }
-    expect(thermostat.currentTemp).toEqual(powersaveMaxTemp)
-  })
-  
-  it('has a maximum temperature of 32 when in not in powersave mode', () => {
-    MaxTemp = thermoStat.MAX_TEMP
-    thermostat.powersave = false
-    for (let i = 0; i < 13; i++){
-      thermostat.up()
-    }
-    expect(thermostat.currentTemp).toEqual(MaxTemp)
-  }) 
+	it("has a maximum temperature of 25 when in powersave mode", () => {
+		powersaveMaxTemp = thermoStat.POWER_SAVE_MAX_TEMP;
+		for (let i = 0; i < 6; i++) {
+			thermostat.up();
+		}
+		expect(thermostat.currentTemp).toEqual(powersaveMaxTemp);
+	});
 
-  it('can turn power save off', () => {
-    thermostat.switchPowerSaveOff()
-    expect(thermostat.powersave).toBe(false)
-  })
+	it("has a maximum temperature of 32 when in not in powersave mode", () => {
+		MaxTemp = thermoStat.MAX_TEMP;
+		thermostat.powersave = false;
+		for (let i = 0; i < 13; i++) {
+			thermostat.up();
+		}
+		expect(thermostat.currentTemp).toEqual(MaxTemp);
+	});
 
-  it('can reset  to start temperature with reset()', () => {
-    thermostat.currentTemp = 22
-    thermostat.resetTemp()
-    expect(thermostat.currentTemp).toEqual(thermoStat.START_TEMPERATURE)
-  })
-    
+	it("can turn power save off", () => {
+		thermostat.switchPowerSaveOff();
+		expect(thermostat.powersave).toBe(false);
+	});
+
+	it("can reset to start temperature with reset()", () => {
+		thermostat.currentTemp = 22;
+		thermostat.resetTemp();
+		expect(thermostat.currentTemp).toEqual(thermoStat.START_TEMPERATURE);
+	});
+
+	it("can return the current energy usage", () => {
+		thermostat.down();
+		thermostat.down();
+		thermostat.down();
+		thermostat.currentEnergyUsage();
+		expect(thermostat.energyUsage).toEqual("low-usage");
+	});
+
+	it("returns high-usage when the tmeperature is above 25", () => {
+		thermostat.switchPowerSaveOff();
+		for (let i = 0; i < 6; i++) {
+			thermostat.up();
+		}
+		console.log(thermostat.currentTemp);
+		thermostat.currentEnergyUsage();
+		expect(thermostat.energyUsage).toEqual("high-usage");
+	});
 });
--- src/thermoStat.js
+++ src/thermoStat.js
@@ -1,37 +1,43 @@
 class thermoStat {
-  
-  constructor(Temperature = thermoStat.START_TEMPERATURE) {
-    this.currentTemp = Temperature
-    this.minimumTemp = thermoStat.MINIMUM_TEMPERATURE
-    this.powersave = true
-  }
+	constructor(Temperature = thermoStat.START_TEMPERATURE) {
+		this.currentTemp = Temperature;
+		this.minimumTemp = thermoStat.MINIMUM_TEMPERATURE;
+		this.powersave = true;
+		this.energyUsage = "medium-usage";
+	}
 
-  up() {
-    if (this.powersave && this.currentTemp < thermoStat.POWER_SAVE_MAX_TEMP) {
-      this.currentTemp++
-    }
-    else if (!this.powersave && this.currentTemp < thermoStat.MAX_TEMP) {
-      this.currentTemp++
-    }
+	up() {
+		if (this.powersave && this.currentTemp < thermoStat.POWER_SAVE_MAX_TEMP) {
+			this.currentTemp++;
+		} else if (!this.powersave && this.currentTemp < thermoStat.MAX_TEMP) {
+			this.currentTemp++;
+		}
+	}
 
-  }
+	down() {
+		if (this.currentTemp > this.minimumTemp) {
+			this.currentTemp--;
+		}
+	}
 
-  down() {
-    if (this.currentTemp > this.minimumTemp) {
-      this.currentTemp--
-    }
-  }
-  
-  switchPowerSaveOff() { 
-    this.powersave = false
-  }
+	switchPowerSaveOff() {
+		this.powersave = false;
+	}
 
-  resetTemp() { 
-    this.currentTemp = thermoStat.START_TEMPERATURE
-  }
-  
-  static START_TEMPERATURE = 20;
-  static MINIMUM_TEMPERATURE = 10;
-  static POWER_SAVE_MAX_TEMP = 25;
-  static MAX_TEMP = 32;
+	resetTemp() {
+		this.currentTemp = thermoStat.START_TEMPERATURE;
+	}
+
+	currentEnergyUsage() {
+		if (this.currentTemp < 18) {
+			return (this.energyUsage = "low-usage");
+		} else if (this.currentTemp > 25) {
+			return (this.energyUsage = "high-usage");
+		}
+	}
+
+	static START_TEMPERATURE = 20;
+	static MINIMUM_TEMPERATURE = 10;
+	static POWER_SAVE_MAX_TEMP = 25;
+	static MAX_TEMP = 32;
 }
